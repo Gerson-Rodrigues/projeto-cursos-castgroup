f5ec1017abbe893f4507893593861023fe1adfa0
Gerson-Rodrigues <rodrigues87gerson@gmail.com>
2022-07-07 01:18:31 -0300

    primeiro commit

---
+++ src/app/cursos/cursos/cursos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../model/curso';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent implements OnInit {
+
+  cursos: Curso[] = [
+    { _id: "1", name:'Angular', categoria:'Front-End'},
+    { _id: "2", name:'Java', categoria: 'Back-End'}
+  ];
+  displayedColumns = ['name', 'categoria'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
