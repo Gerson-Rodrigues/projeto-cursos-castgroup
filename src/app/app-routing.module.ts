1e1eb405b6666dfc351787984cde0c3019e3a244
Gerson-Rodrigues <rodrigues87gerson@gmail.com>
2022-07-09 08:57:56 -0300

    commit 08:57 09/07

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {path: '', pathMatch:'full', redirectTo: 'cursos'},
-  {path: 'cursos', loadChildren:()=> import('./cursos/cursos.module').then( m => m.CursosModule)}
+  {path: 'cursos', loadChildren:()=> import('./cursos/cursos.module').then( m => m.CursosModule)},
 ];
 
 @NgModule({
